27bb8af9800954e6754290bef60c07cbbf7bea4f
Parth526 <parthingle526@gmail.com>
2025-06-01 12:40:26 +0530

    First Commit

---
+++ src/main/java/com/spring/PasswordSecurityService.java
@@ -0,0 +1,15 @@
+package com.spring;
+
+import java.util.Base64;
+import java.util.Base64.Encoder;
+
+public class PasswordSecurityService {
+	
+	public String 	encode(String text) {
+		Encoder encoder = Base64.getEncoder();
+		String encodeToString = encoder.encodeToString(text.getBytes());
+		
+		return encodeToString;
+	}
+
+}

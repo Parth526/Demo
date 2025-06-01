a2fc2aaa23a983fc4d50ad4f7b645919c9eb0713
Parth526 <parthingle526@gmail.com>
2025-06-01 23:40:23 +0530

    Bug-101 also fixed

--- Customer.java
+++ Customer.java
@@ -1,10 +1,17 @@
 class Customer{
 	public static void main(String [] args){
 		
-		
+		// BUG-101 fixing
+		int a = 10;
+		int b = 20;
+		// logic is in progress
+
+
 		// Bug 102 Fixing
 		System.out.println("This was important bug then 101");
 		
+
+
 	}
 	
 }
